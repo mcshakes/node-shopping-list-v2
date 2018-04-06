09b234247bc12cf4c6cf00ee6629a1578b4947ba
Edward McCarthy <efmccarthy26@gmail.com>
2018-04-06 15:37:16 -0600

    Properly posting now, totally forgot the jsonParser middleware, so spent a bunch of time looking at undefined errors

--- server.js
+++ server.js
@@ -9,30 +9,22 @@ const {ShoppingList, Recipes} = require('./models');
 const jsonParser = bodyParser.json();
 const app = express();
 
-// log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-// adding some recipes to `Recipes` so there's something
-// to retrieve.
 Recipes.create(
   'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
 Recipes.create(
   'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
-// when the root of this router is called with GET, return
-// all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
 app.post('/shopping-list', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -51,6 +43,23 @@ app.post('/shopping-list', jsonParser, (req, res) => {
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+app.post("/recipes", jsonParser, (req,res) => {
+  const requiredFields = ["name", "ingredients"];
+
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+
+    const recipe = Recipes.create(req.body.name, req.body.ingredients);
+    res.status(201).json(recipe);
+  }
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
